95bbbacd757c427602785cfcf893e3c753c2e0e1
Tof <tof.aliev@ru.axept.com>
2022-11-09 02:59:41 +0600

    Added base project configurations

--- threatintel/threatintel/wsgi.py
+++ threatintel/threatintel/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for threatintel project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
-"""
-
 import os
 
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'threatintel.settings')
+os.environ.setdefault("DJANGO_CONFIGURATION", "BaseConfiguration")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "threatintel.settings")
 
 application = get_wsgi_application()
